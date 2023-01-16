907b374ab33f94cc311925483dc589a5432f9883
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-01-16 18:35:21 +0900

    Prepare for dev environment

--- lib/weighted_rand.rb
+++ lib/weighted_rand.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "weighted_rand/version"
+require_relative 'weighted_rand/version'
 
 module WeightedRand
-  class Error < StandardError; end
+  class Error < StandardError
+  end
   # Your code goes here...
 end
